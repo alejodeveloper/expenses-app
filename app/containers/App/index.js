a2d06fb60186f962f6fe6485228d2b807d088b10
Alejo <alejo.pm92@gmail.com>
2020-07-14 10:18:58 -0500

    [update] edit nginx config and index app

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -8,20 +8,44 @@
  */
 
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 import GlobalStyle from '../../global-styles';
 
+const AddExpensePage = () => (
+  <div>
+    Add expenses here
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    Edit expenses here
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    Help page here
+  </div>
+);
+
+
 export default function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/create" component={AddExpensePage} />
+          <Route exact path="/edit" component={EditExpensePage} />
+          <Route exact path="/faq" component={HelpPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </BrowserRouter>
       <GlobalStyle />
     </div>
   );
