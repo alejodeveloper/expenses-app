9f600d3ac6c8e5fa32fe80ff3fd98069b12c99d5
Alejo <alejo.pm92@gmail.com>
2020-07-16 21:52:01 -0500

    [update] fix commons

--- app/commons.js
+++ app/commons.js
@@ -3,7 +3,3 @@ Commons files for cosnstants shared accross the web app in general
 */
 
 export const API_URL = `http://localhost:8001`
-export let HEADERS = {
-    Accept: `application/json`,
-    Authorization: "",
-}
